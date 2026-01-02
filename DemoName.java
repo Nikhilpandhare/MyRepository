397b6f49c372e685d4a1f7042836d8e294c7484d
Nikhilpandhare <nikhilpandhare2222@gmail.com>
2026-01-02 12:51:54 +0530

    Update DemoName.java
    Creating a class file to check the various locaters 
    locating the web elements and passing data 
    it perform as same we commands it
    the web link is taking us on proper page or any other site 
    Entering the data like - int  and char 
    using the ID , Name , X path , Class Name , Tag Name ,Link Text.
    checking how it perform it 
    it accurate past data where it commands.

--- DemoName.java
+++ DemoName.java
@@ -1,4 +1,4 @@
-package Locaters;   //(name locater)
+package Locaters;   
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -7,34 +7,24 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class DemoName {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		//to launch browser
         WebDriver driver=new ChromeDriver();
 		
 		driver.get("https://tutorialsninja.com/demo/index.php?route=account/register");
-	
-		//to Maximize window 
+	 
 		driver.manage().window().maximize();
 		
-		//to locate first Name texbox-- by name locater
 		driver.findElement(By.name("firstname")).sendKeys("NIKHIL");
 		
-		//to locate last Name texbox-- by name locater
 		driver.findElement(By.name("lastname")).sendKeys("Abcd");
 		
-		//to locate Email texbox-- by name locater
 		driver.findElement(By.name("email")).sendKeys("Nikhil123@email.com");
 				
-		//to locate Telephone texbox-- by name locater
 		driver.findElement(By.name("telephone")).sendKeys("1234567890");
 		
-		//to locate PassWorld texbox -- by name locater
 	    driver.findElement(By.name("password")).sendKeys("xyz123");
 				
-		//to locate ConfirmPassWorld texbox-- by name locater
-	    driver.findElement(By.name("confirm")).sendKeys("xyz123");
-				
+	    driver.findElement(By.name("confirm")).sendKeys("xyz123");		
 	}
-
 }
+
